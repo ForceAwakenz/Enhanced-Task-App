a796075c849e47b894d65917cadda0b19b8aded9
= <=>
2021-12-02 19:27:06 +0200

    add basic data-table

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -17,10 +17,85 @@ export class CourseComponent implements OnInit, AfterViewInit {
 
     course:Course;
 
+    lessons = [
+        {
+            id: 1,
+            "description": "Angular Tutorial For Beginners - Build Your First App - Hello World Step By Step",
+            "duration": "4:17",
+            "seqNo": 1,
+            courseId: 1
+        },
+        {
+            id: 2,
+            "description": "Building Your First  Component - Component Composition",
+            "duration": "2:07",
+            "seqNo": 2,
+            courseId: 1
+        },
+        {
+            id: 3,
+            "description": "Component @Input - How To Pass Input Data To an  Component",
+            "duration": "2:33",
+            "seqNo": 3,
+            courseId: 1
+        },
+        {
+            id: 4,
+            "description": " Component Events - Using @Output to create custom events",
+            "duration": "4:44",
+            "seqNo": 4,
+            courseId: 1
+        },
+        {
+            id: 5,
+            "description": " Component Templates - Inline Vs External",
+            "duration": "2:55",
+            "seqNo": 5,
+            courseId: 1
+        },
+        {
+            id: 6,
+            "description": "Styling  Components - Learn About Component Style Isolation",
+            "duration": "3:27",
+            "seqNo": 6,
+            courseId: 1
+        },
+        {
+            id: 7,
+            "description": " Component Interaction - Extended Components Example",
+            "duration": "9:22",
+            "seqNo": 7,
+            courseId: 1
+        },
+        {
+            id: 8,
+            "description": " Components Tutorial For Beginners - Components Exercise !",
+            "duration": "1:26",
+            "seqNo": 8,
+            courseId: 1
+        },
+        {
+            id: 9,
+            "description": " Components Tutorial For Beginners - Components Exercise Solution Inside",
+            "duration": "2:08",
+            "seqNo": 9,
+            courseId: 1
+        },
+        {
+            id: 10,
+            "description": " Directives - Inputs, Output Event Emitters and How To Export Template References",
+            "duration": "4:01",
+            "seqNo": 10,
+            courseId: 1
+        }
+    ];
+
     constructor(private route: ActivatedRoute,
                 private coursesService: CoursesService) {
 
     }
+
+    displayedColumns = ['seqNo', 'description', 'duration'];
 
     ngOnInit() {
 
