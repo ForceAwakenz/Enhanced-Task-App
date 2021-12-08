af03ab2f5d0d0f1340a90564fa6e9491b899d8a1
Kiril Vedmidskiy <djimeilsobachka@gmail.com>
2021-12-08 18:24:12 +0200

    fix remove redundant material modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DockTestSortComponent } from './main/dock-test-sort/dock-test-sort.component';
-import { DockTestComponent } from './main/dock-test/dock-test.component';
 import { MainComponent } from './main/main/main.component';
 
 const routes: Routes = [
   { path: '', component: MainComponent},
-  { path: 'test', component: DockTestComponent},
-  { path: 'sort-test', component: DockTestSortComponent},
 ];
 
 @NgModule({
