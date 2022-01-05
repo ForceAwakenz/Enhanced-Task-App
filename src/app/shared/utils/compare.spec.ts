13054ec571b2d00aa85277036241d19a943c4276
Kiril Vedmidskiy <djimeilsobachka@gmail.com>
2022-01-05 19:43:23 +0200

    feat add sortData function tests

--- src/app/shared/utils/compare.spec.ts
+++ src/app/shared/utils/compare.spec.ts
@@ -3,11 +3,11 @@ import { compare } from "./compare";
 describe('compare function',() => {
 
   it('should correctly compare two numbers', () => {
-    expect(compare(1, 2, true)).toBe(1);
+    expect(compare(1, 2, false)).toBe(1);
   });
 
   it('should correctly compare two strings', () => {
-    expect(compare('1', '2', true)).toBe(1);
+    expect(compare('1', '2', false)).toBe(1);
   });
 
 })
--- src/app/task-app-general/task-list/task-list.component.spec.ts
+++ src/app/task-app-general/task-list/task-list.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { provideMockStore } from '@ngrx/store/testing';
 import { AppInitState } from 'src/app/redux/task-app-general.reducers';
+import { taskList } from 'src/app/redux/task-app-general.selectors';
 
 import { TaskListComponent } from './task-list.component';
 
@@ -20,14 +21,31 @@ describe('TaskListComponent', () => {
     fixture = TestBed.createComponent(TaskListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    component.sortedTaskList = [
+      {text: '2', date: new Date().toLocaleDateString(), isDone: false, id: +(new Date())},
+      {text: '1', date: new Date().toLocaleDateString(), isDone: false, id: +(new Date())},
+    ];
+    component.taskList = [
+      {text: '1', date: new Date().toLocaleDateString(), isDone: false, id: +(new Date())},
+      {text: '2', date: new Date().toLocaleDateString(), isDone: false, id: +(new Date())},
+    ];
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should execute sortData function', () => {
-    // component.sortData()
+  it('should execute sortData function correctly', () => {
+    component.sortData({direction: 'asc', active: '1'});
+    component.taskList.map(task => task.text).slice().sort().forEach( (taskText, index) =>
+      expect(taskText).toEqual(component.sortedTaskList[index].text)
+    );
+    expect(component.taskList).toEqual(component.sortedTaskList);
   });
 
+  it('should quit sortData function in unactive sort property', () => {
+    component.sortData({direction: 'asc', active: ''});
+    expect(component.taskList).toEqual(component.sortedTaskList);
+  });
 });
