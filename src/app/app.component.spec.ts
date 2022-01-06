3c90df01f4979ff6d5aa569d48e3deab511e7667
Kiril Vedmidskiy <djimeilsobachka@gmail.com>
2022-01-06 16:13:11 +0200

    fix all remining DI issues with tests, now tests run without any errors

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,13 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { EffectsModule } from '@ngrx/effects';
+import { Store, StoreModule } from '@ngrx/store';
+import { provideMockStore } from '@ngrx/store/testing';
 import { AppComponent } from './app.component';
+import { AppInitState } from './redux/task-app-general.reducers';
+import { STORAGE_SERVICE } from './shared/models/StorageService';
 import { TaskAppGeneralModule } from './task-app-general/task-app-general.module';
-import { TaskAppGeneralComponent } from './task-app-general/task-app-general/task-app-general.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -10,10 +15,17 @@ describe('AppComponent', () => {
       imports: [
         RouterTestingModule,
         TaskAppGeneralModule,
+        StoreModule.forRoot({}),
+        EffectsModule.forRoot([])
       ],
       declarations: [
         AppComponent,
       ],
+      providers: [ 
+        provideMockStore({initialState: AppInitState}),
+        {provide: STORAGE_SERVICE, useValue: { storeTaskList: () => {} } },
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
   });
 
--- src/app/task-app-general/task-app-general/task-app-general.component.spec.ts
+++ src/app/task-app-general/task-app-general/task-app-general.component.spec.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { provideMockStore } from '@ngrx/store/testing';
 import { MatModule } from 'src/app/mat/mat.module';
+import { AppInitState } from 'src/app/redux/task-app-general.reducers';
 import { TaskInputComponent } from '../task-input/task-input.component';
 import { TaskListComponent } from '../task-list/task-list.component';
 import { ToolbarComponent } from '../toolbar/toolbar.component';
@@ -20,6 +22,9 @@ describe('TaskAppGeneralComponent', () => {
         ToolbarComponent,
       ],
       imports: [MatModule, FormsModule, ReactiveFormsModule, CommonModule],
+      providers: [
+        provideMockStore({initialState: AppInitState}),
+      ]
     })
     .compileComponents();
   });
